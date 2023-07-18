1a3c10eacd8229fffa15f61a2b9ec26c6e63fdbf
myelectronix <myelectronix@mail.ru>
2023-07-18 17:44:28 +0000

    setting of uuid, public and private key has been added

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,29 @@
 LOCKFILE=.lockfile
 if [ ! -f $LOCKFILE ]
 then
-echo "NoFile"
 
 #generate uuid
+echo "Generate UUID..."
 /opt/xray/xray uuid >> /opt/xray/uuid
 
+
 #generate Public & Private keys
+echo "Generate public & private keys..."
 /opt/xray/xray x25519 >> /opt/xray/keys
 
 #Create files with Public & Private keys
 awk '/Public/{print $3}' keys >> public
 awk '/Private/{print $3}' keys >> private
 
+UUID=$(cat uuid)
+PRIVATE=$(cat private)
+
+#set uuid in config.json
+sed -i 's/"id":.*/"id": "'${UUID}'",/' config.json
+
+#set private key in config.json
+sed -i 's/"privateKey":.*/"privateKey": "'${PRIVATE}'",/' config.json
 
 touch $LOCKFILE
 fi
-
 
